dd62ce0970e41e01347ef24880e4b0692072c769
j-greenberg <jgreenberg@runbox.com>
2020-04-24 17:48:29 -0400

    updated

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
 const express = require("express"); 
 const server = express(); 
-
+const helmet = require("helmet"); 
 
 server.use(express.json()); 
+server.use(helmet()); 
 
 module.exports = server; 
